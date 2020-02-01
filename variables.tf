9bc549d7453b157ebbaa44786bbcd95fc760434a
ClaudeSeo <ehdaudtj@gmail.com>
2020-02-02 02:42:35 +0900

    chore: EKS 설정 정보 추가

--- variables.tf
+++ variables.tf
@@ -2,26 +2,30 @@ variable region {
     default = "ap-northeast-2"
 }
 
+variable "available_az" {
+    default = [
+        "ap-northeast-2a",
+        "ap-northeast-2c"
+    ]
+}
+
 variable name {
     default = "eks-vpc"
 }
 
-variable vpc_cidr_blocks {
+variable "vpc_cidr_blocks" {
     default = "10.0.0.0/16"
 }
-
-variable public1_subnet_cidr_blocks {
-    default = "10.0.1.0/24"
+variable "public_cidr_blocks" {
+    default = [
+        "10.0.1.0/24",
+        "10.0.2.0/24"
+    ]
 }
 
-variable public2_subnet_cidr_blocks {
-    default = "10.0.2.0/24"
-}
-
-variable private1_subnet_cidr_blocks {
-    default = "10.0.10.0/24"
-}
-
-variable private2_subnet_cidr_blocks {
-    default = "10.0.11.0/24"
+variable "private_cidr_blocks" {
+    default = [
+        "10.0.10.0/24",
+        "10.0.11.0/24"
+    ]
 }
--- vpc.tf
+++ vpc.tf
@@ -10,42 +10,29 @@ resource "aws_vpc" "vpc" {
 }
 
 # === Public Subnet ===
-resource "aws_subnet" "public_subnet1" {
-    vpc_id = aws_vpc.vpc.id
-    availability_zone = "ap-northeast-2a"
-    cidr_block = var.public1_subnet_cidr_blocks
-    map_public_ip_on_launch = false
-    tags = {
-        Name = "${var.name}-public-subnet-1"
-    }
-}
+resource "aws_subnet" "public_subnet" {
+    count = 2
 
-resource "aws_subnet" "public_subnet2" {
     vpc_id = aws_vpc.vpc.id
-    availability_zone = "ap-northeast-2c"
-    cidr_block = var.public2_subnet_cidr_blocks
+    availability_zone = var.available_az[count.index]
+    cidr_block = var.public_cidr_blocks[count.index]
     map_public_ip_on_launch = false
     tags = {
-        Name= "${var.name}-public-subnet-2"
+        Name = "${var.name}-public-subnet-${count.index}"
+        "kubernetes.io/cluster/${var.name}" = "shared"
     }
 }
 
 # === Private Subnet ===
-resource "aws_subnet" "private_subnet1" {
-    vpc_id = aws_vpc.vpc.id
-    availability_zone = "ap-northeast-2a"
-    cidr_block = var.private1_subnet_cidr_blocks
-    tags = {
-        Name = "${var.name}-private-subnet-1"
-    }
-}
+resource "aws_subnet" "private_subnet" {
+    count = 2
 
-resource "aws_subnet" "private_subnet2" {
     vpc_id = aws_vpc.vpc.id
-    availability_zone = "ap-northeast-2c"
-    cidr_block = var.private2_subnet_cidr_blocks
+    availability_zone = var.available_az[count.index]
+    cidr_block = var.private_cidr_blocks[count.index]
     tags = {
-        Name = "${var.name}-private-subnet-2"
+        Name = "${var.name}-private-subnet-${count.index}"
+        "kubernetes.io/cluster/${var.name}" = "shared"
     }
 }
 
@@ -67,7 +54,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
     allocation_id = aws_eip.nat_eip.id
-    subnet_id = aws_subnet.public_subnet1.id
+    subnet_id = aws_subnet.public_subnet[0].id
     tags = {
         "Name": var.name
     }
@@ -89,24 +76,16 @@ resource "aws_route_table" "private" {
 }
 
 # === Route Table Association ===
-resource "aws_route_table_association" "public_subnet1" {
-  subnet_id = aws_subnet.public_subnet1.id
-  route_table_id = aws_vpc.vpc.default_route_table_id
+resource "aws_route_table_association" "public_subnet" {
+    count = 2
+    subnet_id = aws_subnet.public_subnet[count.index].id
+    route_table_id = aws_vpc.vpc.default_route_table_id
 }
 
-resource "aws_route_table_association" "public_subnet2" {
-  subnet_id = aws_subnet.public_subnet2.id
-  route_table_id = aws_vpc.vpc.default_route_table_id
-}
-
-resource "aws_route_table_association" "private_subnet1" {
-  subnet_id = aws_subnet.private_subnet1.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_subnet2" {
-  subnet_id = aws_subnet.private_subnet2.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "private_subnet" {
+    count = 2
+    subnet_id = aws_subnet.private_subnet[count.index].id
+    route_table_id = aws_route_table.private.id
 }
 
 # === Route ===
